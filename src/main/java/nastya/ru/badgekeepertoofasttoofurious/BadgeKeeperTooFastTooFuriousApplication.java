a9f2df534790368c3dd0ff246c43b161b4d40600
kannstas <k.annstas@gmail.com>
2024-04-24 16:24:37 +0300

    Initial commit

---
+++ src/main/java/nastya/ru/badgekeepertoofasttoofurious/BadgeKeeperTooFastTooFuriousApplication.java
@@ -0,0 +1,13 @@
+package nastya.ru.badgekeepertoofasttoofurious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadgeKeeperTooFastTooFuriousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadgeKeeperTooFastTooFuriousApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nastya/ru/badgekeepertoofasttoofurious/BadgeKeeperTooFastTooFuriousApplicationTests.java
@@ -0,0 +1,13 @@
+package nastya.ru.badgekeepertoofasttoofurious;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadgeKeeperTooFastTooFuriousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
